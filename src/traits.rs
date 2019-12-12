a42e7557d776dc94ad33dde2a3e843cc585bd1fe
oliver <oliver.giersch@googlemail.com>
2019-12-12 11:38:09 +0100

    trait API adjustments and docs

--- src/traits.rs
+++ src/traits.rs
@@ -33,9 +33,12 @@ pub unsafe trait GenericReclaimer: Reclaimer {
 
 pub unsafe trait Reclaimer: Default + Sync + Sized + 'static {
     type Global: Default + Sync + Sized;
+    // TODO: type Handle<'local, 'global>: ReclaimerHandle<Reclaimer = Self> + 'local + 'global;
     type Header: Default + Sync + Sized + 'static;
 
     fn new() -> Self;
+    // fn owning_local_handle<'global>(&'global self) -> Self::Handle<'_, 'global>;
+    // unsafe fn raw_local_handle(&self) -> Self::Handle<'_, '_>;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -54,6 +57,8 @@ pub unsafe trait ReclaimerHandle: Clone + Sized {
 // Protect (trait)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/// A trait for implementing guard types associated with a specific
+/// [`Reclaimer`].
 pub unsafe trait Protect: Clone + Sized {
     /// The associated memory reclaimer.
     type Reclaimer: Reclaimer;
@@ -64,12 +69,21 @@ pub unsafe trait Protect: Clone + Sized {
     /// [`ProtectRegion`].
     fn release(&mut self);
 
+    /// Loads and protects the value currently stored in `atomic` and returns
+    /// an optional protected [`Shared`] reference.
+    ///
+    /// `protect` takes an [`Ordering`] argument ...
     fn protect<T, N: Unsigned>(
         &mut self,
         src: &Atomic<T, Self::Reclaimer, N>,
         order: Ordering,
     ) -> MaybeNull<Shared<T, Self::Reclaimer, N>>;
 
+    /// Loads and protects the value currently stored in `atomic` if it equals
+    /// the `expected` pointer and returns an optional protected [`Shared`]
+    /// reference.
+    ///
+    /// `protect_if_equal` takes and [`Ordering`] argument ...
     fn protect_if_equal<T, N: Unsigned>(
         &mut self,
         src: &Atomic<T, Self::Reclaimer, N>,
@@ -82,4 +96,6 @@ pub unsafe trait Protect: Clone + Sized {
 // ProtectRegion (trait)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/// An extension trait for guard types of [`Reclaimer`]s that protect entire
+/// regions of code rather than individual [`Atomic`] pointers.
 pub unsafe trait ProtectRegion: Protect {}
